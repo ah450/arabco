428c542a726be14afcd195f72321a422563e61da
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2016-10-25 00:11:21 +0200

    adds polyfills and generic styles

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 angular.module('arabco', ['ui.router', 'ui.router.title',
     'arabcoTemplates', 'ngAnimate']);
 
-
 angular.module('arabco').config(function($compileProvider) {
   return $compileProvider.debugInfoEnabled(false);
+});
+
+angular.module('arabco').config(function($urlMatcherFactoryProvider) {
+  return $urlMatcherFactoryProvider.strictMode(false);
 });
